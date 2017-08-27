441a92dc7a16c5b21b13173ccc63acfdaf7a504a
bromondo <31172342+bromondo@users.noreply.github.com>
2017-08-27 14:16:52 -0700

    messing around with ploting

--- Main.py
+++ Main.py
@@ -6,8 +6,11 @@ import pandas_datareader.data as web
 
 style.use('ggplot')
 
-start = dt.datetime(2000,1,1)
-end = dt.datetime(2017,8,27)
-
-df = web.DataReader('TSLA', 'yahoo',start, end)
-print(df.tail(10))
+##start = dt.datetime(2000,1,1)
+##end = dt.datetime(2017,8,27)
+##df = web.DataReader('TSLA', 'yahoo',start, end)
+df = pd.read_csv('tsla.csv')
+print(df.head())
+print(df[['High','Low']].head())
+df['High'].plot(kind='bar')
+plt.show()
