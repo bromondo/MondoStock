0179db63250397ec00f69fa91971771dbe28b2e9
bromondo <31172342+bromondo@users.noreply.github.com>
2017-08-27 20:51:01 -0700

    Working with Graphing with plt grids

--- Main.py
+++ Main.py
@@ -10,7 +10,13 @@ style.use('ggplot')
 ##end = dt.datetime(2017,8,27)
 ##df = web.DataReader('TSLA', 'yahoo',start, end)
 df = pd.read_csv('tsla.csv')
+
 print(df.head())
-print(df[['High','Low']].head())
-df['High'].plot(kind='bar')
+df['100ma'] = df['Adj Close'].rolling(window=100).mean()
+print(df.tail())
+
+ax1 = plt.subplot2grid(6,1,0,0,rowspan=5,colspan=1)
+ax2 = plt.subplot2grid(6,1,5,0)
+
+ax1.plot(df['Date'], df['100ma'])
 plt.show()
