cd3216c6eecc30d9e9082a6839595c990d096946
bromondo <31172342+bromondo@users.noreply.github.com>
2017-08-27 20:03:44 -0700

    Setting up the middle ware for the WebFrontStock App

---
+++ MondoStockWeb/WebFrontStock/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class WebfrontstockConfig(AppConfig):
+    name = 'WebFrontStock'
---
+++ MondoStockWeb/WebFrontStock/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("<H2>YO!</h2>")
+
+# Create your views here.
