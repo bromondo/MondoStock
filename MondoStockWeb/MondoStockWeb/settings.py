5ab9259f88cec4909a03a418ab20ab7626735aed
bromondo <31172342+bromondo@users.noreply.github.com>
2017-08-27 19:37:03 -0700

    removed k

--- MondoStockWeb/MondoStockWeb/settings.py
+++ MondoStockWeb/MondoStockWeb/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ugpz2i)+x9uez@rfd=t0chlmjc-1b63=lvjc_n)uk$h3%vw-e+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
