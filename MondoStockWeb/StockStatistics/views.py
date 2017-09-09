218a7a8a467939b1eed766347b7695f2f3015395
bromondo <31172342+bromondo@users.noreply.github.com>
2017-09-08 22:15:44 -0700

    trying to get date from the user using urllib

--- MondoStockWeb/StockStatistics/views.py
+++ MondoStockWeb/StockStatistics/views.py
@@ -1,10 +1,15 @@
 import datetime as dt
+import urllib.request
 from django.shortcuts import render
 import pandas as pd
 import pandas_datareader.data as web
 start = dt.datetime(2000,1,1)
 end = dt.datetime(2017,8,27)
+from lxml import html
 
+page = urllib.request.urlopen('http://localhost')
+htm = page.text()
+print(htm)
 def index(request):
     return render(request, 'StockStatistics/home.html')
 
