ad0b523a22409735a560528a62eb7d05bd9d6951
bromondo <31172342+bromondo@users.noreply.github.com>
2017-09-09 21:01:22 -0700

    using Javascript to get a Date from the user

--- MondoStockWeb/StockStatistics/views.py
+++ MondoStockWeb/StockStatistics/views.py
@@ -1,15 +1,12 @@
 import datetime as dt
 import urllib.request
+import requests
 from django.shortcuts import render
 import pandas as pd
 import pandas_datareader.data as web
 start = dt.datetime(2000,1,1)
 end = dt.datetime(2017,8,27)
-from lxml import html
 
-page = urllib.request.urlopen('http://localhost')
-htm = page.text()
-print(htm)
 def index(request):
     return render(request, 'StockStatistics/home.html')
 
